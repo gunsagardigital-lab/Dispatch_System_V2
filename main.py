d4ac7b096941e25eab7f06e2691a4018d489a699
gunsagardigital-lab <gunsagardigital@gmail.com>
2026-09-08 05:05:54 +0530

    main.py

--- main.py
+++ main.py
@@ -509,6 +509,7 @@ elif page == "🔐 Admin Panel":
                 if st.form_submit_button("💾 डेटा सेव करें"):
                     if mode == "नई एंट्री करें" and not veh: 
                         st.error("गाड़ी नंबर अनिवार्य है!")
+                        st.cache_data.clear()
                     else:
                         t_row = len(df_adm) + 2
                         row_data = [
